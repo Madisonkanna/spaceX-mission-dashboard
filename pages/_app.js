d86d8537d53a844e5b76193e9065568c7ddf3916
Madison <madisonkanna@gmail.com>
2022-10-03 10:48:06 -0700

    chore: lint

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import '../styles/globals.css';
-import { ApolloProvider } from '@apollo/client';
-import apolloClient from '../graphql';
+import "../styles/globals.css";
+import { ApolloProvider } from "@apollo/client";
+import apolloClient from "../graphql";
 
 function MyApp({ Component, pageProps }) {
   return (
